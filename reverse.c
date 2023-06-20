cf3c8d1092ec212bf544fc3b4d500236ae56ae30
Utkarsh Kumar <utkarsh6629@gmail.com>
2023-06-20 21:05:06 +0530

    getline complete

--- reverse.c
+++ reverse.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+int maxlength=1000;
 int getline(char str[],int max){
-    int c,i
+    int c,i=0;
+    for(i=0;--max>0&&(c=getchar())!=EOF&&c!='\n';i++)
+        str[i]=c;
+        i++;
+    if(c=='\n')
+        str[i++]=c;
+    str[i]='\0'
 }
