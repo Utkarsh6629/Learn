b8650c5797c87ab69c67ee7f5b84070f30a6e62a
Utkarsh Kumar <utkarsh6629@gmail.com>
2023-07-13 08:37:04 +0530

    moved to learn

--- learn/dupe2.c
+++ learn/dupe2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int arr[10]={1,2,3,4,5,4,3,1};
+    int arr[10]={1,2,3,4,5,4,3,1 };
     int size=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size&&arr[i]!=0;i++){
         for(int j=i+1;j<size;j++){
