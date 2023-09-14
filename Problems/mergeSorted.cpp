6af888ca7eaf3a30e85aa62de174cec3cfdc8c1a
Utkarsh Kumar <utkarsh6629@gmail.com>
2023-09-14 19:04:45 +0530

    fixed by re-coding the whole thing

--- Problems/mergeSorted.cpp
+++ Problems/mergeSorted.cpp
@@ -1,30 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void merge(int a[],int n,int b[],int m,int arr[]){
-
+void mergeS(int a[],int s1, int b[], int s2,int c[]){
     int i=0,j=0,k=0;
-    while(i<n&&j<m){
+    while(i<s1 && j <s2){
         if(a[i]>b[j])
-            arr[k++]=b[j++];
-        else   
-            arr[k++]=a[i++];
-        }
-    while(i<n){
-        arr[k++]=a[i++];
+            c[k++]=b[j++];
+        else  
+            c[k++]=a[i++];
     }
-    while(j<m){
-        arr[k++]=b[i++];
+    while(i<s1){
+        c[k++]=a[i++];
+    }
+    while(j<s2){
+        c[k++]=b[j++];
     }
 }
+
 
 int main(){
     int arr1[5]={1,3,5,7,9};
     int arr2[4]={2,4,6,8};
-    int arr[9]={0};
-    merge(arr1,5,arr2,5,arr);
+    int arr[9];
+    mergeS(arr1,5,arr2,4,arr);
     for(int i=0;i<9;i++){
-    cout<<arr[i]<<endl;
+        cout<<arr[i]<<" ";
     }
-
 }
